5cbdcb32b17398deba2c1bc763e0a5e4f1a65613
KamilLee <parousia0918@gmail.com>
2021-04-20 01:52:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -61,4 +61,4 @@ def recommend():
 
 if __name__ == '__main__':
         port = int(os.environ.get("PORT", 5000))
-        app.run(host="127.0.0.1", port=port, debug=True)
+        app.run(host="0.0.0.0", port=port, debug=True)
